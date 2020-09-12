8b50598be1c91227172a6128e394f56909d4aa9b
Marc <g40419@gmail.com>
2020-09-12 20:28:32 +0800

    feat: 新增 css/pages/NotFound.scss，將style 移入檔案

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,15 +4,6 @@
   </div>
 </template>
 
-<style lang="scss">
-.container {
-  background-image: url("../assets/image/background.jpeg");
-  background-size: cover;
-  height: 100%;
-  h1 {
-    background: rgba(0, 0, 0, 0.812);
-    color: white;
-    font-size: 4rem;
-  }
-}
+<style lang="scss" scoped>
+  @import '../assets/css/pages/NotFound.scss';
 </style>
