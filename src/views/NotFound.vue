2e60d26dbf69d4e55915520b03e50049211d47e0
Marc <g40419@gmail.com>
2020-09-12 19:44:17 +0800

    reset css

---
+++ src/views/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+  <h1>404NotFound</h1>
+  </div>
+</template>
+
+<style lang="scss">
+.container {
+  background-image: url("../assets/image/background.jpeg");
+}
+</style>
--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,20 @@
+// 引入jest 方法
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
 
+//引入要測試的檔案
+
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+//開始寫測試的內容
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
+    // 用 shallowMount(HelloWorld) 
+    // 現在 vm === wrapper.vm
+    
+    // const wrapper = shallowMount(HelloWorld, {
+    //   propsData: { msg }
+    // })
     expect(wrapper.text()).toMatch(msg)
   })
 })
