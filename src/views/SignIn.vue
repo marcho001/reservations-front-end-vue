7f21234f3e5ed0ed2f03ce2bc481c0698fd65377
Marc <g40419@gmail.com>
2020-09-12 21:37:56 +0800

    feat: 1. creat views/SignIn.vue 2. 簡易切版

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,6 +1,56 @@
 <template> 
-  <div class="container">
-    <div class="left-section"></div>
-    <div class="right-section"></div>
+  <div 
+    class="container d-flex justify-content-center align-items-center">
+    <div class="form-section p-4">
+      <form>
+        <div class="form-section_title mt-3">
+          <h1>登入</h1>
+        </div>
+        <div class="form-section_group mt-5">
+          <div class="form-section_group-input d-flex flex-column">
+            <label for="email">Email</label>
+            <input 
+              type="email" 
+              name="email" 
+              id="email">
+          </div>
+          <div class="form-section_group-input mt-3 d-flex flex-column">
+            <label for="password">Password</label>
+            <input 
+              type="password" 
+              name="Password" 
+              id="Password">
+          </div>
+          <div 
+            class="form-section_group-button mt-3">
+            <button type="submit">立即登入</button>
+          </div>
+        </div>
+      </form>
+    <div class="footer-section mt-5">
+      <p>
+        尚未註冊嗎？
+        <a href="#">立即註冊</a>
+      </p>
+    </div>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .container {
+    background-image: url("../assets/image/signin.jpg");
+    background-size: cover;
+    height: 100%;
+    .form-section {
+      // height: 60%;
+      width: 80%;
+      border: 1px solid #000;
+      background: rgba(255, 255, 255, 0.856);
+    }
+  }
+  @media screen and (min-width: 768px) {
+    .container {
+      align-items: flex-start;
+    }
+  }
+</style>
