21e8171f2a93633c30b1f2abcffafdb04150550a
Marc <g40419@gmail.com>
2020-09-13 14:18:35 +0800

    feat: 1. 將代辦列出 2. 將資料用 v-model 綁定:

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -8,11 +8,22 @@
         <div class="form-section_group mt-5">
           <div class="form-section_group-input d-flex flex-column">
             <label for="email">Email :</label>
-            <input class="mt-2 px-2" type="email" name="email" id="email" required/>
+            <input 
+              v-model="email"
+              class="mt-2 px-2" 
+              type="email" 
+              name="email" id="email" 
+              required/>
           </div>
           <div class="form-section_group-input mt-3 d-flex flex-column">
             <label for="password">Password :</label>
-            <input class="mt-2 px-2" type="password" name="Password" id="Password" required/>
+            <input 
+              v-model="password"
+              class="mt-2 px-2" 
+              type="password" 
+              name="Password" 
+              id="Password" 
+              required/>
           </div>
           <div class="form-section_group-button mt-5 d-flex flex-column align-items-center">
             <button class="p-1" type="submit">立即登入</button>
@@ -30,7 +41,19 @@
 </template>
 
 <script>
-
+// 向 api post 登入
+// 將token 存到 localstorage
+// 使用者資料存到 vuex
+// 轉跳到首頁
+export default {
+  data () {
+    return {
+      email: '',
+      password: '',
+      isProcessing: false
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
