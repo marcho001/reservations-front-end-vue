e61d52276e0410e65479f5684987f1395a73dc0b
Marc <g40419@gmail.com>
2020-09-18 20:36:58 +0800

    設定假資料，將 restaurantId 傳給createComment component“

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -6,7 +6,9 @@
       <div class="restaurant_comments">
         <h1>評論</h1>
         <hr />
-        <CreateCommentForm :font-awesome="icon" />
+        <CreateCommentForm 
+          :font-awesome="icon" 
+          :restaurant-id="restaurant.id"/>
         <br />
         <RestaurantComments :font-awesome="icon" />
       </div>
@@ -26,6 +28,21 @@ import RestaurantDetail from '../components/RestaurantPage/RestaurantDetail'
 import CreateCommentForm from '../components/RestaurantPage/CreateCommentForm'
 import RestaurantComments from '../components/RestaurantPage/RestraurantComments'
 
+const fakeRest = {
+    id: 3,
+    name: '龍蝦吃到吐',
+    rating: 4.2,
+    address: '台北市中山區明水路3000號',
+    image: '',
+    description: '',
+    Category: {
+      id: 1,
+      name: '海鮮料理'
+    },
+    Comments: {}
+  }
+
+
 export default {
   components: {
     RestaurantDetail,
@@ -35,8 +52,32 @@ export default {
   },
   data() {
     return {
-      icon: { faStar, farStar, faMapMarkedAlt }
+      icon: { faStar, farStar, faMapMarkedAlt },
+      restaurant: {
+        id: 0,
+        name: '',
+        rating: 0,
+        address: '',
+        image: '',
+        description: '',
+        Category: {
+          id: 0,
+          name: ''
+        },
+        Comments: {}
+      }
     }
+  },
+  methods: {
+    fetchRestaurant() {
+      this.restaurant = {
+        ...this.restaurant,
+        ...fakeRest
+      }
+    }
+  },
+  created() {
+    this.fetchRestaurant()
   }
 }
 </script>
