f929b7664bfc552bc257cb22a27d124527d1586d
Marc <g40419@gmail.com>
2020-09-29 19:29:27 +0800

    Refactory: 用 transition 做評論轉場:

--- src/components/RestaurantPage/CreateCommentForm.vue
+++ src/components/RestaurantPage/CreateCommentForm.vue
@@ -1,12 +1,12 @@
 <template>
   <form
     @submit.prevent.stop="handleSubmit"
-    class="restaurant_comments_newComment"
+    class="newComment"
   >
     <div class="d-flex align-items-center">
       <h3>留下評論</h3>
       <div
-        class="restaurant_comments_newComment--rating d-flex justify-content-around"
+        class="newComment_rating d-flex justify-content-around"
       >
         <label
           class="cursor-pointer"
@@ -28,25 +28,26 @@
       </div>
     </div>
     <br />
-    <div
-      v-if="rating > 0"
-      class="restaurant_comments_newComment--comment w-100 "
-    >
-      <textarea
-        v-model.trim="comment"
-        class="text"
-        name="comment"
-        rows="4"
-        autofocus
-        wrap="hard"
-      ></textarea>
-      <div class="d-flex">
-        <button class="submit m-3 px-2" type="submit">新增</button>
-        <button @click.prevent.stop="cancelComment" class="cancel m-3 px-2">
-          取消
-        </button>
-      </div>
-    </div>
+    <transition name="show">
+      <div
+        v-show="rating > 0"
+        class="newComment_comment w-100 "
+      >
+        <textarea
+          v-model.trim="comment"
+          class="text"
+          name="comment"
+          rows="4"
+          wrap="hard"
+        ></textarea>
+        <div class="d-flex">
+          <button class="submit m-3 px-2" type="submit">新增</button>
+          <button @click.prevent.stop="cancelComment" class="cancel m-3 px-2">
+            取消
+          </button>
+        </div>
+      </div>  
+    </transition>
   </form>
 </template>
 
