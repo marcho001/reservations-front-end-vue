623c9c3243dd5b9cc4f216906ce37a2c2ac6c076
Marc <g40419@gmail.com>
2020-10-01 15:52:35 +0800

    Refactory: 將navbar 和 historyOrder 拆成components

---
+++ src/components/MemberPage/MemberNavbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="navbar">
+      <div class="navbar_wrapper d-flex align-items-center justify-content-around">
+        <a class="navbar_wrapper_item" href="#">我的訂單</a>
+        <a class="navbar_wrapper_item" href="#">修改會員資料</a>
+      </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import '@/assets/css/components/MemberPage/MemberNavbar';
+</style>
--- src/views/Member.vue
+++ src/views/Member.vue
@@ -3,148 +3,30 @@
     <div class="space-40"></div>
     <div class="member_wrapper">
 
-     <div class="navbar">
-      <div class="navbar_wrapper d-flex align-items-center justify-content-around">
-        <a class="navbar_wrapper_item" href="#">我的訂單</a>
-        <a class="navbar_wrapper_item" href="#">修改會員資料</a>
-      </div>
-    </div>
+     <MemberNavbar />
 
-    <div class="right_section wrapper">
-      <h1 class="text-center">我的訂單</h1>
-      <div class="space-40"></div>
-      <div class="category">
-        <div class="category_wrapper d-flex justify-content-around">
-          <a class="category_wrapper_item m-2 active" href="#">即將到來</a>
-          <a class="category_wrapper_item m-2" href="#">歷史訂單</a>
-          <a class="category_wrapper_item m-2" href="#">未付款訂單</a>
-        </div>
-      </div>
-      <hr />
-      <div class="order">
-        <div class="item p-2 my-4">
-          <div class="item_title d-flex justify-content-between">
-            <div>
-              <h2 class="mb-2">龍蝦吃到吐</h2>
-              <p>編號：hiahifhiehaisehiashef</p>
-            </div>
-            <p class="align-self-end">已付款</p>
-          </div>
-          <hr />
-          <div class="item_detail">
-            <ul class="bill_list_item d-flex align-items-center my-2">
-              <li class="name text-center m-1">秋刀魚抹鹽梯 黃金鱸魚</li>
-              <li class="m-1">x2</li>
-              <li class="border mx-3"></li>
-              <li class="m-1">$ 100</li>
-            </ul>
-            <h3 class="text-right">總計： $200</h3>
-          </div>
-          <hr />
-          <div class="item_footer d-flex justify-content-around">
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約時間：<span>18:30</span></div>
-            <div class="m-2">付款方式：<span>信用卡</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-          </div>
-        </div>
+      <div class="wrapper">
 
-
-        <div class="item p-2 my-4">
-          <div class="item_title d-flex justify-content-between">
-            <div>
-              <h2 class="mb-2">龍蝦吃到吐</h2>
-              <p>編號：hiahifhiehaisehiashef</p>
-            </div>
-            <p class="align-self-end">已付款</p>
-          </div>
-          <hr />
-          <div class="item_detail">
-            <ul class="bill_list_item d-flex align-items-center my-2">
-              <li class="name text-center m-1">秋刀魚抹鹽梯 黃金鱸魚</li>
-              <li class="m-1">x2</li>
-              <li class="border mx-3"></li>
-              <li class="m-1">$ 100</li>
-            </ul>
-            <h3 class="text-right">總計： $200</h3>
-          </div>
-          <hr />
-          <div class="item_footer d-flex justify-content-around">
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約時間：<span>18:30</span></div>
-            <div class="m-2">付款方式：<span>信用卡</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-          </div>
-        </div>
-        <div class="item p-2 my-4">
-          <div class="item_title d-flex justify-content-between">
-            <div>
-              <h2 class="mb-2">龍蝦吃到吐</h2>
-              <p>編號：hiahifhiehaisehiashef</p>
-            </div>
-            <p class="align-self-end">已付款</p>
-          </div>
-          <hr />
-          <div class="item_detail">
-            <ul class="bill_list_item d-flex align-items-center my-2">
-              <li class="name text-center m-1">秋刀魚抹鹽梯 黃金鱸魚</li>
-              <li class="m-1">x2</li>
-              <li class="border mx-3"></li>
-              <li class="m-1">$ 100</li>
-            </ul>
-            <h3 class="text-right">總計： $200</h3>
-          </div>
-          <hr />
-          <div class="item_footer d-flex justify-content-around">
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約時間：<span>18:30</span></div>
-            <div class="m-2">付款方式：<span>信用卡</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-          </div>
-        </div>
-        <div class="item p-2 my-4">
-          <div class="item_title d-flex justify-content-between">
-            <div>
-              <h2 class="mb-2">龍蝦吃到吐</h2>
-              <p>編號：hiahifhiehaisehiashef</p>
-            </div>
-            <p class="align-self-end">已付款</p>
-          </div>
-          <hr />
-          <div class="item_detail">
-            <ul class="bill_list_item d-flex align-items-center my-2">
-              <li class="name text-center m-1">秋刀魚抹鹽梯 黃金鱸魚</li>
-              <li class="m-1">x2</li>
-              <li class="border mx-3"></li>
-              <li class="m-1">$ 100</li>
-            </ul>
-            <h3 class="text-right">總計： $200</h3>
-          </div>
-          <hr />
-          <div class="item_footer d-flex justify-content-around">
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約時間：<span>18:30</span></div>
-            <div class="m-2">付款方式：<span>信用卡</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-            <div class="m-2">預約日期：<span>9/11</span></div>
-          </div>
-        </div>
+        <MemberHistoryOrder />
 
       </div>
-    </div>
     </div>
     
     
    
   </div>
 </template>
+<script>
+import MemberHistoryOrder from '../components/MemberPage/MemberHistoryOrder'
+import MemberNavbar from '../components/MemberPage/MemberNavbar'
+
+export default {
+  components: {
+    MemberNavbar,
+    MemberHistoryOrder
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 @import '@/assets/css/pages/Member';
