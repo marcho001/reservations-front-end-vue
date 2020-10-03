5c2f694414a6f677b8087e11234cd75f17589557
Marc <g40419@gmail.com>
2020-10-03 14:37:15 +0800

    調整樣式

---
+++ src/views/BusinessCenter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="space-40"></div>
+    <div class="business_wrapper">
+      <UserNavTab 
+        :tabs="tabs"/>
+        <div class="wrapper">
+
+        
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import UserNavTab from '../components/UserNavTab'
+export default {
+  components: {
+    UserNavTab
+  },
+  data () {
+    return {
+      tabs: [
+        {
+          name: '編輯餐廳',
+          pathName: 'business',
+          paramsName: 'restaurant'
+        },
+        {
+          name: '編輯菜單',
+          pathName: 'business',
+          paramsName: 'menu'
+        },
+        {
+          name: '訂位表',
+          pathName: 'business',
+          paramsName: 'calendar'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+@media screen and (min-width: 768px) {
+  .business_wrapper {
+    display: flex;
+  }
+  .wrapper {
+    height: 100vh;
+    overflow: scroll;
+    scroll-behavior: smooth;
+  }
+}
+</style>
