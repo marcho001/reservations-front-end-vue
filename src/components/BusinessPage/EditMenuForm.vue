42d7a7aa1f8bcbb691b320d56b16ceadaa205e2a
Marc <g40419@gmail.com>
2020-10-03 17:20:09 +0800

    完成編輯餐點切版

---
+++ src/components/BusinessPage/EditMenuForm.vue
@@ -0,0 +1,75 @@
+<template>
+  <div 
+    @click.self="toggleEditForm"
+    class="menu_form d-flex justify-content-center align-items-center position-fixed">
+        <form class="form p-4 scroll">
+          <label class="form_item image position-relative" for="image">
+            <img 
+            :src="'' | emptyImage" />
+            <p class="image_icon text-center cursor-pointer">
+              <font-awesome-icon :icon="solidIcon.faCameraRetro" />
+            </p>
+          </label>
+          <input class="d-none" type="file" id="image" />
+          <div class="form_item">
+      <label for="name">餐點名稱：</label>
+      <input type="text" id="name" />
+    </div>
+    <div class="form_item">
+    <label for="category">餐點類別：</label>
+      <select class="px-1" id="category">
+        <option value="" disabled selected>請選擇類別</option>
+        <option value="">牛肉</option>
+        <option value="">豬肉</option>
+      </select>
+    </div>
+    <div class="form_item">
+      <label for="description">
+      餐點描述：<br> 0/50
+      
+      </label>
+      <textarea class="p-2" id="description" rows="3"></textarea>
+    </div>
+    <div class="form_item">
+      <label for="price">售價：</label>
+      <input type="number" id="price" />
+    </div>
+    <div class="form_item">
+      <label for="status">是否上架：
+        <input type="checkbox" id="status" />
+      </label>
+    </div>
+    <div class="d-flex justify-content-end">
+      <button class="form_save mt-2">
+        儲存
+      </button>
+    </div>
+        </form>
+      </div>
+</template>
+
+<script>
+import { emptyImageFilter } from '../../utils/mixin'
+import { FontAwesomeIcon, solid } from '../../utils/icon'
+
+export default {
+  components: {
+    FontAwesomeIcon
+  },
+  data () {
+    return {
+      solidIcon: solid
+    }
+  },
+  mixins: [emptyImageFilter],
+  methods: {
+    toggleEditForm () {
+      this.$emit('after-toggle-edit-form')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/css/components/BusinessPage/EditMenuForm';
+</style>
--- src/views/BusinessCenter.vue
+++ src/views/BusinessCenter.vue
@@ -6,7 +6,43 @@
         :tabs="tabs"/>
         <div class="wrapper">
 
-      <EditRestaurant />
+      <EditRestaurant 
+        v-if="nowPage === 'restaurant'"/>
+      
+      <div>
+      <button class="create m-4">新增餐點</button>
+      <hr>
+
+        <div class="menu d-grid">
+          <div class="menu_item">
+          <img :src="meal.image | emptyImage" />
+          <div class="menu_item_body">
+            <h1 class="text-center">
+              eateat
+            </h1>
+            <p class="description text-center p-2">
+              apsdofiaosdfj joi j oi ijoij oj oi oijo ioj ijoj oi oijioj ojoi o joijojojojo  jojoj o
+            </p>
+          </div>
+          <div
+            class="menu_item_footer d-flex justify-content-around align-items-center my-3"
+          >
+            <h3 class="mr-3">NT$ 30</h3>
+            <button class="sale">販賣中</button>
+            <button class="stop">停售中</button>
+            
+          </div>
+        </div>
+
+        </div>
+      
+      <EditMenuForm 
+        @after-toggle-edit-form="afterToggleEditForm"
+        v-show="editMenu"/>
+
+      </div>
+
+
       </div>
 
     </div>
@@ -14,15 +50,23 @@
 </template>
 
 <script>
+import { emptyImageFilter } from '../utils/mixin'
 import UserNavTab from '../components/UserNavTab'
 import EditRestaurant from '../components/BusinessPage/EditRestaurant'
+import EditMenuForm from '../components/BusinessPage/EditMenuForm'
 export default {
   components: {
     UserNavTab,
-    EditRestaurant
+    EditRestaurant,
+    EditMenuForm
   },
   data () {
     return {
+      nowPage: 'menu',
+      editMenu: false,
+      meal: {
+        image: ''
+      },
       tabs: [
         {
           name: '編輯餐廳',
@@ -41,6 +85,12 @@ export default {
         }
       ]
     }
+  },
+  mixins: [emptyImageFilter],
+  methods: {
+    afterToggleEditForm () {
+      this.editMenu = false
+    }
   }
 }
 </script>
@@ -54,5 +104,28 @@ export default {
   overflow: scroll;
   scroll-behavior: smooth;
 }
+
+.menu {
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  grid-gap: 2rem;
+  &_item {
+  @extend %shadow, %radius;
+  overflow: hidden;
+  &_body {
+    border-bottom: 1px solid $mask;   
+    }
+  }
+}
+.create {
+  @include buttonStyle($buttonColor, $buttonColor, $white)
+}
+.sale {
+  @include buttonStyle($categoryTag, $categoryTag, $white)
+}
+.stop {
+  @include buttonStyle($mainColor, $mainColor, $white)
+}
+
+
 
 </style>
