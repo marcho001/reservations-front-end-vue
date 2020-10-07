3544f7568e28a298ae2c8d95b1f28625d302c915
Marc <g40419@gmail.com>
2020-10-07 16:46:21 +0800

    Feat: 1. 將menu 抽換成 component 帶入 2. 利用 v-bind:is 依照route 帶入畫面

---
+++ src/components/BusinessPage/Menu.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button @click="toggleEditForm" class="create m-4">
+      新增餐點
+    </button>
+    <hr />
+
+    <div class="menu d-grid">
+      <MenuCard />
+    </div>
+
+    <EditMenuForm
+      @after-toggle-edit-form="afterToggleEditForm"
+      v-show="editMenu"
+    />
+  </div>
+</template>
+<script>
+import MenuCard from './MenuCard'
+import EditMenuForm from './EditMenuForm'
+
+export default {
+  components: {
+    MenuCard,
+    EditMenuForm
+  },
+  data () {
+    return {
+      editMenu: false
+    }
+  },
+  methods: {
+    afterToggleEditForm() {
+      this.editMenu = false
+    },
+    toggleEditForm() {
+      this.editMenu = true
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.menu {
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  grid-gap: 2rem;
+}
+
+.create {
+  @include buttonStyle($buttonColor, $buttonColor, $white)
+}
+</style>
