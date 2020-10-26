8958dd0bdcdc778b2d9038fa781d1493a12d0d6b
Marc <g40419@gmail.com>
2020-10-26 15:15:26 +0800

    Feat: Spinner 完成

---
+++ src/components/Spinner.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="spinner">
+    <div class="bouncing-loader">
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import '@/assets/css/components/Spinner';
+</style>
